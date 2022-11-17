60e714554dc6dafd83387c8bb40213c25c13ded3
KitsuneTanuki <117897906+KitsuneTanuki@users.noreply.github.com>
2022-11-17 16:30:20 +0700

    Delete UG9_C_71220830.py

--- UG9_C_71220830.py
+++
@@ -1,5 +0,0 @@
-Nama = input("Masukkan nama Anda : ")
-Kuliah = input("Masukkan mata kuliah : ")
-Grup = input ("Masukkan grup : ")
-print("Haloo!",Nama)
-print(Nama[0:9],"tergabung dalam kelas",Kuliah,"pada grup",Grup)
