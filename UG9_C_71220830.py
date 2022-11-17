f2b9be385ec31c7727f720f97a722b3af24d3f70
KitsuneTanuki <117897906+KitsuneTanuki@users.noreply.github.com>
2022-11-17 16:11:58 +0700

    Add files via upload

---
+++ UG9_C_71220830.py
@@ -0,0 +1,5 @@
+Nama = input("Masukkan nama Anda : ")
+Kuliah = input("Masukkan mata kuliah : ")
+Grup = input ("Masukkan grup : ")
+print("Haloo!",Nama)
+print(Nama[0:9],"tergabung dalam kelas",Kuliah,"pada grup",Grup)
