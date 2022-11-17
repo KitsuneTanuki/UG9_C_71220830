e2dba7e6028cb53d7022f78e984d5e5a6bec42ec
KitsuneTanuki <117897906+KitsuneTanuki@users.noreply.github.com>
2022-11-17 16:28:55 +0700

    Add files via upload

---
+++ 2_C_71220830.py
@@ -0,0 +1,5 @@
+Kec = int(input("Masukkan kecepatan tempuh : "))
+Waktu = int(input("Masukkan waktu yang diperlukan : "))
+
+print("Teman Anda mengisi bensin sebanyak",Kec*Waktu//10)
+print("Biaya yang dikeluarkan untuk mengisi bensin adalah",Kec*Waktu//10*15000)
